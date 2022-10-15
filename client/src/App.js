81117c523b9f0c4edb2199f9bafde77ea91dad53
Daniel <danmorales2287@gmail.com>
2022-10-15 18:38:34 -0500

    cleaning up tutorial code

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const [data, setData] = React.useState("hello");
+  const [data, setData] = React.useState('hello');
 
   const getTest = async () => {
     try {
@@ -10,22 +10,32 @@ function App() {
         method: 'GET',
         header: {
           Accept: 'application/json',
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         }
       });
       return await res.json();
     } catch (err) {console.log(err);};
   };
 
+  // const getRoute = async () => {
+  //   try {
+  //     const res = fetch ('url', {
+  //       addresses
+  //     })
+  //     return res.json
+  //   } catch (err) {console.log(err);};
+  // }
+
   React.useEffect(() => {
     getTest().then(res => {
       setData(res.message);
     }).catch(err => console.log(err));
-  })
+  });
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>{data}</h1>
+      {/* <div>{route}</div> */}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
-require("dotenv").config();
+require('dotenv').config();
 
 
 // app
@@ -14,20 +14,32 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log('DB CONNECTED')).catch(err => console.log("DB CONNECTION ERROR", err));
+}).then(() => console.log('DB CONNECTED')).catch(err => console.log('DB CONNECTION ERROR', err));
     
 
 // middleware
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors({origin: true, credentials: true }));
 
+
 // routes
-const testRoutes = require("./routes/test")
-app.use("/", testRoutes);
+const router = express.Router();
+
+router.get('/test', async (req, res) => {
+    res.status(200).json({
+        message: 'Test  API is working!',
+    });
+});
+
+//router.get('/route', async)
+
+// use the routes
+app.use('/', router);
 
 
 // port
 const port = process.env.PORT || 8080;
+
 
 // listener
 const server = app.listen(port, () => console.log(`Server is running on port: ${port}`));
